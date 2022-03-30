a5997edf5c0e79650c49ac1f9985a72a292214fb
Axzurey <66486198+Axzurey@users.noreply.github.com>
2022-03-30 08:42:05 -0400

    fix rocast

--- src/shared/athena/utils.ts
+++ src/shared/athena/utils.ts
@@ -1,11 +1,32 @@
 namespace utils {
-    interface classDef<T> {
-        new(...args: any[]): T;
-      }
 
-    export function propertyExistsInClass<c extends classDef<any>, p extends string>(classlike: c, property: p): classlike is c & {property: any} {
+    export type propertyExists<P extends string, T> = { [k in P]: T}
+
+    export type valueof<T, P extends keyof T> = T[P];
+
+    export function propertyExistsInObject<c extends any, p extends string, propType>(classlike: c, property: p): classlike is c & propertyExists<p, propType> {
         if (classlike[property as unknown as keyof typeof classlike]) return true;
         return false;
+    }
+
+    export namespace array {
+        export function partition<T>(arr: T[], start: number, end: number){
+            // Taking the last element as the pivot
+            const pivotValue = arr[end];
+            let pivotIndex = start; 
+            for (let i = start; i < end; i++) {
+                if (arr[i] < pivotValue) {
+                // Swapping elements
+                [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+                // Moving to next element
+                pivotIndex++;
+                }
+            }
+            
+            // Putting the pivot value in the middle
+            [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]] 
+            return pivotIndex;
+        };
     }
 }
 
--- src/shared/zero/space.ts
+++ src/shared/zero/space.ts
@@ -1,4 +1,4 @@
-import { propertyExistsInClass } from "shared/athena/utils";
+import { propertyExistsInObject } from "shared/athena/utils";
 import component from "./basic/component";
 import entity from "./basic/entity";
 
@@ -11,14 +11,32 @@ namespace space {
             if (index) return index;
             return [];
         }
-        export function filterAllWithPropertyAs<V>(property: string, value: V, components?: component[]) {
-            if (components) {
-                components.forEach((v) => {
-                    if (propertyExistsInClass(v, property) && v[property] === value) {
-                        
+        /**
+         * 
+         * @param property 
+         * @param value 
+         * @param useComponents if this is not provided, it searches through every entity in the game
+         * @returns 
+         */
+        export function filterAllWithPropertyAs<V>(property: string, value: V, useComponents?: component[]) {
+            let selected: component[] = [];
+            if (useComponents) {
+                useComponents.forEach((v) => {
+                    if (propertyExistsInObject(v, property) && v[property] === value) {
+                        selected.push(v)
                     }
                 })
             }
+            else {
+                entityMaps.forEach((comparr) => {
+                    comparr.forEach((v) => {
+                        if (propertyExistsInObject(v, property) && v[property] === value) {
+                            selected.push(v)
+                        }
+                    })
+                })
+            }
+            return selected;
         }
     }
 }
